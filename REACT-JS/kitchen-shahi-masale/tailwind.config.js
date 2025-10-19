f0cc5689fe2311c48b3f33fb64b5f8a5cc946597
chetan-there <therechetan24@gmail.com>
2025-10-19 18:45:50 +0530

    DAY - 13

---
+++ REACT-JS/kitchen-shahi-masale/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        saffron: "#FF6F00",
+        spiceGreen: "#1A4D2E",
+        masalaRed: "#8B0000",
+      },
+    },
+  },
+  plugins: [],
+};
